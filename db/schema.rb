645596b170a5d7e00b6aa50303f9d851e9e6f49f
Franck <falcou.franck@gmail.com>
2016-12-13 16:21:23 +0000

    header v1.1

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20161213145920) do
-
-ActiveRecord::Schema.define(version: 20161213100757) do
 
   create_table "prank_types", force: :cascade do |t|
     t.string   "name"
@@ -22,7 +19,6 @@ ActiveRecord::Schema.define(version: 20161213100757) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
-
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false

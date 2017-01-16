6e4028b0b3b03a23484244002540dedb4e48c923
Franck <jeremy.petit3@free.fr>
2017-01-16 15:26:30 +0000

    PapperClipLetters

--- app/models/letter.rb
+++ app/models/letter.rb
@@ -1,4 +1,6 @@
 class Letter < ActiveRecord::Base
   belongs_to :type_letter
+  has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/pictures/missing.jpg"
+  validates_attachment_content_type :photo, content_type: /\Aimage\/.*\z/
   validates :name, :description, presence: true
 end
